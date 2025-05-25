952d2fce45d6d88f51f8ee07f40ee1f5b554e76b
fathurohmansf <fathurrhm909@gmail.com>
2025-05-25 12:31:44 +0700

    add route Skill user & final Skill create,get,update,delete

--- src/routes/cvRoutes.js
+++ src/routes/cvRoutes.js
@@ -12,6 +12,10 @@ import { createProject } from "../controllers/cv/project/createProject";
 import { updateProject } from "../controllers/cv/project/updateProject";
 import { deleteProject } from "../controllers/cv/project/deleteProject";
 import { getProject } from "../controllers/cv/project/getProject";
+import { createSkill } from "../controllers/cv/skill/createSkill";
+import { updateSkill } from "../controllers/cv/skill/updateSkill";
+import { deleteSkill } from "../controllers/cv/skill/deleteSkill";
+import { getSkill } from "../controllers/cv/skill/getSkill";
 
 
 const cvRoute = new Router();
@@ -40,13 +44,13 @@ cvRoute.put("/api/cv/project/:id", validateMiddleUser, updateProject);
 cvRoute.delete("/api/cv/project/:id", validateMiddleUser, deleteProject);
 cvRoute.get("/api/cv/project", validateMiddleUser, getProject);
 
-// // =========================
-// // Skill
-// // =========================
-// cvRoute.post("/api/cv/skill", validateMiddleUser, createSkill);
-// cvRoute.put("/api/cv/skill/:id", validateMiddleUser, updateSkill);
-// cvRoute.delete("/api/cv/skill/:id", validateMiddleUser, deleteSkill);
-// cvRoute.get("/api/cv/skill", validateMiddleUser, getSkill);
+// =========================
+// Skill
+// =========================
+cvRoute.post("/api/cv/skill", validateMiddleUser, createSkill);
+cvRoute.put("/api/cv/skill/:id", validateMiddleUser, updateSkill);
+cvRoute.delete("/api/cv/skill/:id", validateMiddleUser, deleteSkill);
+cvRoute.get("/api/cv/skill", validateMiddleUser, getSkill);
 
 // // =========================
 // // Certification
