436fea310e766eaa5a60f15ca7a66f34495f927c
fathurohmansf <fathurrhm909@gmail.com>
2025-05-25 12:41:50 +0700

    get cv Certification user

---
+++ src/controllers/cv/certification/getCertification.js
@@ -0,0 +1,37 @@
+import { request,response } from "express";
+import db from "../../../connector";
+import { getProfileIdByUserId } from "../../../utils/findProfileId";
+
+async function getCertification(req = request, res = response) {
+  const userId = req.userId;
+  try {
+    const profileID = await getProfileIdByUserId(userId);
+
+    const certifications = await db.certifications.findMany({
+      where: {
+        profileId: profileID,
+      },
+      select: {
+        id: true,
+        name: true,
+        issu_by: true,
+        year: true,
+        id_credetial_url: true,
+        description: true,
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: certifications,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+}
+
+export { getCertification };
