c3257c6dd81a981883eff0d0f8f37baa523439ba
fathurohmansf <fathurrhm909@gmail.com>
2025-05-27 17:19:18 +0700

    fix static image BE

--- src/server.js
+++ src/server.js
@@ -29,7 +29,7 @@ app.use(
 
 app.use(
   "/public",
-  express.static(path.join(__dirname, "../public/img_ProfileUser"))
+  express.static(path.join(__dirname, "../public/imageProfile"))
 )
 
 app.use(
